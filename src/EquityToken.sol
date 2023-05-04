c2d69e56bbbbc28ce977a9cc2e4358401989c423
MatinR1 <matingr1375@yahoo.com>
2023-05-04 12:10:05 +0430

    InfraFund

---
+++ src/EquityToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./IEquityToken.sol";
+contract EquityToken is IEquityToken, ERC20, Ownable {
+
+    uint256 constant private INITIAL_SUPPLY = 100_000e18;
+    event Staked(address indexed user, uint256 indexed amount);
+    event Withdrawn(address indexed user, uint256 indexed amount);
+
+    constructor() ERC20("Equity Token", "EQT") {
+
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
