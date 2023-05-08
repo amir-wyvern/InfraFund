c5de1a3c4fb36ef95b6835429218f03a2ce55e22
MatinR1 <matingr1375@yahoo.com>
2023-05-08 22:57:04 +0430

    CharityTokenSync

--- src/Derivatives/CharityToken.sol
+++ src/Derivatives/CharityToken.sol
@@ -2,16 +2,23 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
 import "./ICharityToken.sol";
-contract CharityToken is ICharityToken, ERC20 {
+
+errpr AmountIsZero();
+
+contract CharityToken is ICharityToken, ERC20, Initializable {
 
     address private owner;
+    IERC20 paymentToken;
 
-    event Staked(address indexed user, uint256 indexed amount);
-    event Withdrawn(address indexed user, uint256 indexed amount);
+    mapping(address => uint) private donators;
+
+    event Donated(address indexed user, uint256 indexed amount);
 
     function initialize(
+        IERC20 _paymentToken,
         address _owner,
         string calldata __name,
         string calldata __symbol
@@ -20,7 +27,18 @@ contract CharityToken is ICharityToken, ERC20 {
         initializer
     {
         owner = _owner;
+        paymentToken = _paymentToken;
         _name = __name;
         _symbol = __symbol;
+    }
+
+    function donate(uint amount) external {
+
+        if (amount == 0)
+            revert AmountIsZero();
+
+        donators[msg.sender] == amount;
+        paymentToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit Donated(msg.sender, amount);
     }
 }
